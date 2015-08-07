a2f78b17b5e6dd7c934a0528e08358c8a5063a5a
Milena Quittnat <milena.quittnat@cern.ch>
2015-08-07 12:04:06 +0200

    randomize right extended pdf

--- Analysis/macros/fit_bias.cpp
+++ Analysis/macros/fit_bias.cpp
@@ -6,45 +6,42 @@
 	gSystem->Load("libdiphotonsUtils");
  
 	//TODO includes etc
+	//do each component per category + control
  	TFile* nomfitresfile = new TFile("full_analysis_anv1_v19_2D_split_shapes_semiparam_lumi_5/multidimfit_fit_self.root");
 	TFile* fitpdffile = new TFile("full_analysis_anv1_v19_2D_split_shapes_semiparam_lumi_5/higgsCombine_fit_truth.MultiDimFit.mH0.root");
 	fitpdffile->cd();
-	w->loadSnapshot("MultiDimFit");
+	w->getSnapshot("MultiDimFit");
 	w->exportToCint("ws");
-	//implement also control region and EBEE category like plot_fit.C
-	
-//    RooArgSet* snap = w->getSnapshot("MultiDimFit");
+	RooProdPdf& fitShape=ws::shapeBkg_pp_EBEB;
+	RooProduct & fitNorm=ws::shapeBkg_pp_EBEB__norm;
+	RooExtendPdf* fitPdf=new RooExtendPdf("fitPdf","fitPdf",fitShape,fitNorm);
+	ws::mgg.setRange("sig_region",ws::mgg.getMin(),ws::mgg.getMax());
+	RooPlot * framei = ws::mgg.frame(Title("check"),Bins(134),Range("sig_region"));
+	fitPdf->plotOn(framei,LineColor(kBlue));
+//	fitPdf.Print();
 	//get address of pdfs
-	//TODO get integral
-	 RooAddPdf & randPdf=ws::pdf_binEBEB_nuis;
-	 //cout << randPdf.createIntegral(ws::mgg,"sig_region").getVal()  << endl;
-//	 RooAddPdf* fitPdf=randPdf.Clone("fitPdf");
-  //	RooAddPdf* randPdf=ranPdf->clone("randPdf");
-	//cout << randPdf->getVal(ws::pp_EBEB_frac) << endl;
-//	double test=ws::n_exp_final_binEBEB_proc_pf.getVal();
- //   cout << test << endl; 
+	w->loadSnapshot("MultiDimFit");
+	RooProdPdf & randShape=ws::shapeBkg_pp_EBEB;
+	RooProduct & randNorm=ws::shapeBkg_pp_EBEB__norm;
+	RooExtendPdf* randPdf=new RooExtendPdf("randPdf","randPdf",randShape,randNorm);
  	RooArgSet & obs=RooArgSet(ws::mgg,ws::templateNdim2_unroll);
-	RooArgSet* pdfParams = randPdf.getParameters(obs) ;
-	pdfParams.Print();
+	RooArgSet* pdfParams = randPdf->getParameters(obs) ;
 	//get fitresult and randomize parameters in covariance matrix
 	nomfitresfile->cd();
 	const TMatrixDSym & cov=fit->covarianceMatrix();
 	
 	//const int nsampling=500;
-	const int nsampling=500;
-	//TODO loop to create arrrays
+	const int nsampling=2;
     //const int nbins=191;
 	//get correct number of bins
     const int nbins=2;
 	Double_t intPdfs[nsampling][nbins];
-	ws::mgg.setRange("sig_region",ws::mgg.getMin(),ws::mgg.getMax());
 	for(int r=0;r< nsampling;++r){	
 	//square root method for decomposition automatically applied
 		RooArgList & randParams=fit.randomizePars();
 		*pdfParams = randParams ;
 		Double_t intSig=0.;
 		intSig=randPdf.createIntegral(ws::mgg,"sig_region").getVal()  ;
-//		pdfParams.Print("v");
 	//  plot vs mass
 	  	double start=270.;
 	  //get from dataset nbins
@@ -54,25 +51,20 @@
 			Double_t max= ws::mgg.getMin() + binning*bin +binning;
 	     	ws::mgg.setRange("bin_region",ws::mgg.getMin(),max);
 			intBin=randPdf.createIntegral(ws::mgg,"bin_region").getVal()  ;
-	//		cout << fitPdf->createIntegral(ws::mgg,"sig_region").getVal()  ;
-	//		cout << intBin/intSig << endl;
+			//cout << " intBin " << intBin << " divided by intRange " << intBin/intSig << endl;
 		 	intPdfs[r][bin]=intBin/intSig;
-			if(intPdfs[r][1] < 0.01)
+			/*if(intPdfs[r][1] < 0.01)
 			{
 	    		TCanvas * canvi = new TCanvas("check","check");
 	    		canvi->SetLogy();
 	    		canvi->SetLogx();
-	    		RooPlot * framei = ws::mgg.frame(Title("check"),Bins(134),Range("sig_region"));
-	    		randPdf->plotOn(framei,LineColor(kBlue));
-	    //		fitPdf->plotOn(framei,LineColor(kBlack));
+	    		randPdf->plotOn(framei,LineColor(kBlack));
 	    		framei->Draw();
-	    	//	data->plotOn(framei,DataError(RooAbsData::Poisson));
 			}	
+			*/
 		}
 	}
 //plot result
- 	
-	ws::mgg.setRange("sig_region",ws::mgg.getMin(),ws::mgg.getMax());
 	gStyle->SetOptStat(111111);
 	std::vector< TH1F * > histVec;
 	for(int bin=0;bin< nbins;bin++)
@@ -82,8 +74,8 @@
 			//throw toys to see if works
 			//
 		int nbin=100;
-		Double_t xmin=0.05;
-		Double_t xmax=0.03;
+		Double_t xmin=0.0;
+		Double_t xmax=1.;
 		TH1F* hist=new TH1F("hist","hist",nbin,xmin, xmax);
   		TH1F *cum = new TH1F("cum","cum", nbin, xmin, xmax);
 			
@@ -111,12 +103,9 @@
   		c1->cd(2);
   		cum->Draw();
 }	
-		//TH1* cum=hist->GetCumulative();
-		//cum->Draw("SAME");
 	//	histVec->push_back(hist);
 	//	delete hist;
 		
-		//do check plots
 	}
 	
 }
