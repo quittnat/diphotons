ebc020d033c9266d457cbd224878084f369f725f
Milena Quittnat <milena.quittnat@cern.ch>
2015-10-13 17:45:17 +0200

    add last changes

--- Analysis/macros/control_plots.sh
+++ Analysis/macros/control_plots.sh
@@ -51,14 +51,16 @@ done
 src=$1 && shift
 if [[ $(whoami) == "mquittna" ]]; then
 #	src=/afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_spring15v825ns_sync_v3/ && shift
-	src=/shome/mquittna/CMSSW/EXO_7_4_6_patch2/src/diphotons/Analysis/config/output && shift
-	src=/shome/mquittna/CMSSW/EXO_7_4_6_patch2/src/diphotons/Analysis/macros/full_analysis_spring15_7412v2_runD_v2 && shift
+#	src=/shome/mquittna/CMSSW/EXO_7_4_6_patch2/src/diphotons/Analysis/config/output && shift
+#	src=/shome/mquittna/CMSSW/EXO_7_4_6_patch2/src/diphotons/Analysis/macros/full_analysis_spring15_7412v2_runD_v2 && shift
 #	src=/afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_spring15v825ns_sync_v3/ && shift
+	src=./output2dIDv2_selv1/ && shift
+	
 fi
 
 www=~/www/exo/spring15_v850ns
 if [[ $(whoami) == "mquittna" ]]; then
-    www=/afs/cern.ch/user/m/mquittna/www/diphoton/basic_plots_spring15_7412v2_runD_v2/
+    www=/afs/cern.ch/user/m/mquittna/www/diphoton/basic_plots_spring15_7412v2_runD_v2_selv2/
 ##   www=/afs/cern.ch/user/m/mquittna/www/diphoton/basic_plots_QCD_HT100ToInf_spring1550ns/
 fi
 echo $1
@@ -79,4 +81,5 @@ fi
 ## ./eff_plots.py --load singlepho_eff_plots.json      -O $www/$target/signlepho_efficiency --input-dir $src
 ## ./eff_plots.py --load singlepho_eff_plots.json      -O $www/$target/signlepho_efficiency_grav15  --template "%(sample)s/histograms/RSGravToGG_kMpl_001_M_1500_13TeV_%(cat)s%(name)s"  --input-dir $src 
 ## ./eff_plots.py --load singlepho_eff_plots.json      -O $www/$target/signlepho_efficiency_grav50  --template "%(sample)s/histograms/RSGravToGG_kMpl_001_M_5000_13TeV_%(cat)s%(name)s"  --input-dir $src 
+
 
