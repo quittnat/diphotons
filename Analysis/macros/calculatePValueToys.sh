1b5752af74835d138f2baef8daa9a43a9c7b2914
Milena Quittnat <milena.quittnat@cern.ch>
2016-03-06 22:37:38 +0100

    update spin2

--- Analysis/macros/calculatePValueToys.sh
+++ Analysis/macros/calculatePValueToys.sh
@@ -2,17 +2,17 @@
 set -e
 set -x
 #combineall loops over masses
-coup=$2
-spin=$3
-datacard_bkg="datacard_combined_spin0_wnuis_unblind_grav_$2_950.txt"
-infolder="/shome/mquittna/CMSSW/CMSSW_7_1_5/src/diphotons/Analysis/macros/combined_spin$3_wnuis_unblind_all/"
+coup=001
+datacard_bkg="datacard_combined_spin0_wnuis_unblind_grav_001_950.txt"
+infolder="/shome/mquittna/CMSSW/CMSSW_7_1_5/src/diphotons/Analysis/macros/combined_spin0_wnuis_unblind_all/"
 JOBDIR=sgejob-$JOB_ID
 ls -l /scratch/mquittna
 mkdir -p /scratch/mquittna/$JOBDIR
+ls -l /scratch/mquittna/$JOBDIR
 outfolder=/scratch/mquittna/$JOBDIR
 cp $infolder/datacard*.txt $outfolder/.
 ls -al $outfolder
-toysFile="/shome/mquittna/CMSSW/CMSSW_7_1_5/src/diphotons/Analysis/macros/combined_spin$3_wnuis_unblind_all/higgsCombine_bkgLEE.GenerateOnly.mH0.$1.root"
+toysFile="/shome/mquittna/CMSSW/CMSSW_7_1_5/src/diphotons/Analysis/macros/combined_spin0_wnuis_unblind_all/higgsCombine_bkgLEE.GenerateOnly.mH0.$1.root"
 ntoys=100
 replaceNames=1
 #$ -q all.q
@@ -39,9 +39,10 @@ fi
 #wait
 
 ls -al $toysFile
-$CMSSW_DIR/src/diphotons/Analysis/macros/combineall.sh $outfolder  $coup -M ProfileLikelihood  --toysFile $toysFile  -t $ntoys -n LEE -s $1 --pvalue --significance --cont --hadd 
+####$CMSSW_DIR/src/diphotons/Analysis/macros/combineall.sh $outfolder  $coup -M ProfileLikelihood  --toysFile $toysFile  -t $ntoys -n LEE -s $1 --pvalue --significance  --hadd --cont 
+$CMSSW_DIR/src/diphotons/Analysis/macros/combineall.sh $outfolder  $coup -M ProfileLikelihood  --toysFile $toysFile  -t $ntoys -n LEE -s $1 --pvalue --significance  --hadd 
 ls -al $outfolder
-hadd $outfolder/higgsCombineLEE_kall.ProfileLikelihood.$s.root $outfolder/higgsCombineLEE_k*.ProfileLikelihood.$s.root 
-cp $outfolder/higgsCombineLEE_k*.root $infolder/.
+hadd -f $outfolder/higgsCombineLEE_kall.ProfileLikelihood.$s.root $outfolder/higgsCombineLEE_k*.ProfileLikelihood.$s.root 
+cp $outfolder/higgsCombineLEE_k*.root $infolder/. 
 
 
--- Analysis/macros/combineall.sh
+++ Analysis/macros/combineall.sh
@@ -23,6 +23,11 @@ while [[ -n $1 ]]; do
 	    label=$2
 	    shift
 	    ;;
+	-s)
+		seed=$2
+		args="$args $1 $2"
+		shift
+		;;
 	--hadd)
 	    hadd="hadd"
 	    ;;
@@ -44,34 +49,47 @@ shift
 libs="-L libdiphotonsUtils"
 rootversion=$(root-config --version| tr '.' ' ')
 ##[[ $rootversion -gt 5 ]] && libs="-L libdiphotonsRooUtils"
-
 for coup in $(echo $coupl | tr ',' ' '); do
     cards=datacard*_grav_${coup}_*.txt
     outputs=""
     
     for card in $cards; do
-	echo $card
-	binary=$(echo $card | sed 's%.txt$%.root%')
-	signame=$(echo $card | sed 's%.*grav_%%; s%.txt%%')
-	set $(echo $signame | tr '_' ' ')
-	kmpl=$1
-	mass=$2
-	log=combine_log_${method}_${label}_${kmpl}_${mass}.log
-	set -x
-	if [[ -z $dry ]] && ( [[ -z $cont ]] || [[ ! -f higgsCombine${label}_k${kmpl}.${method}.mH$mass.root ]] ); then 
-	    if [[ -f $binary ]] && [[ $binary -nt $card ]]; then
-		card=$binary
-	    fi
-	    echo combine $libs $args -n "${label}_k${kmpl}" -m $mass $card > $log
-	    combine $libs $args -n "${label}_k${kmpl}" -m $mass $card 2>&1 | tee -a $log
-	    ## sleep 1
-	fi
-	set +x
-	tail -5 $log 
-	[[ -f higgsCombine${label}_k${kmpl}.${method}.mH$mass.root ]] && outputs="$outputs higgsCombine${label}_k${kmpl}.${method}.mH$mass.root"
+		echo $card
+		binary=$(echo $card | sed 's%.txt$%.root%')
+		signame=$(echo $card | sed 's%.*grav_%%; s%.txt%%')
+		set $(echo $signame | tr '_' ' ')
+		kmpl=$1
+		mass=$2
+		#log=combine_log_${method}_${label}_${kmpl}_${mass}.log
+		log=combine_log_$card.log
+		set -x
+	
+		if [[ -n $seed ]]; then 
+			filename=higgsCombine${label}_k${kmpl}.${method}.mH$mass.$seed.root 
+		else
+			filename=higgsCombine${label}_k${kmpl}.${method}.mH$mass.root 
+		fi
+
+		if [[ -z $dry ]] && ( [[ -z $cont ]] || [[ ! -f $filename ]] ); then 
+	    	if [[ -f $binary ]] && [[ $binary -nt $card ]]; then
+			card=$binary
+	    	fi
+	    	#echo combine $libs $args -n "${label}_k${kmpl}" -m $mass $card > $log
+	    	echo combine $libs $args -n "${label}_k${kmpl}" -m $mass $card 
+	    	#combine $libs $args -n "${label}_k${kmpl}" -m $mass $card 2>&1 | tee -a $log
+	    	combine $libs $args -n "${label}_k${kmpl}" -m $mass $card >& /dev/null
+	    	## sleep 1
+		fi
+		set +x
+	#	tail -5 $log 
+		[[ -f $filename ]] && outputs="$outputs $filename"
     done
     if [[ -n $hadd ]]; then
-	hadd -f higgsCombine${label}_k${kmpl}.$method.root $outputs
+		if [[ -n $seed ]]; then 
+			hadd -f higgsCombine${label}_k${kmpl}.$method.$seed.root $outputs
+		else
+			hadd -f higgsCombine${label}_k${kmpl}.$method.root $outputs
+		fi
     fi
 done
 
