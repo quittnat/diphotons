2f61541ca73b043b7387efcca15637124e6e94f5
Milena Quittnat <milena.quittnat@cern.ch>
2016-03-10 15:58:57 +0100

    updated auto plotter jest

--- Analysis/macros/auto_0T.sh
+++ Analysis/macros/auto_0T.sh
@@ -2,12 +2,13 @@
 set -x
 fudge=1.4
 cuts=(0 1 2)
+#cuts=(0)
 
 for cut in "${cuts[@]}" ;do
 	echo $cut
 	if [[ $cut -eq 0 ]]; then
-		output=full_analysis_moriond16v1_0T_sync_v6_230_or_300_500
-		cut='((mass < 500.)'
+		output=full_analysis_moriond16v1_0T_sync_v6_230_or_320_500
+		cut='(mass < 500.)'
 
 	elif [[ $cut -eq 1 ]]; then
 		output=full_analysis_moriond16v1_0T_sync_v6_500_Inf
@@ -28,14 +29,14 @@ for cut in "${cuts[@]}" ;do
 		--cut "$cut" \
     	--output $output/output_mc.root 
 
-  ## merge in Data
+ ## merge in Data
 	./auto_plotter.py --load auto_plotter_histos.json \
 		--selection cic \
 		--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_0T_sync_v6_data/output.root \
-   		--process DoubleEG\*\* \
+  		--process DoubleEG\*\* \
    		--cut "$cut"\
    		--output $output/output_data.root 
 
 	hadd -f $output/output.root $output/output_data.root $output/output_mc.root  
-	./basic_plots.py --load basic_plots_0T.json -O /afs/cern.ch/user/m/mquittna/www/diphoton/$output/ --input-dir $output  --fudge $fudge --lumi 0.6 
+	./basic_plots.py --load basic_plots_0T.json -O /afs/cern.ch/user/m/mquittna/www/diphoton/moriond16/$output/ --input-dir $output  --fudge $fudge --lumi 0.6 
 done
--- Analysis/macros/auto_jet_plot.sh
+++ Analysis/macros/auto_jet_plot.sh
@@ -1,62 +1,48 @@
 #!/bin/bash
 
 set -x
-##fudge=4.5
 fudge=1.4
-
-version=v6
-##output=full_analysis_moriond16v1_sync_v4_0_730
-##cut='(mass < 730.)'
-##output=full_analysis_moriond16v1_sync_v4_500_730
-##cut='(mass > 500 && mass < 730.)'
-output=full_analysis_moriond16v1_sync_v4_730_790
-cut='(mass > 730 && mass < 790)'
-ptmax1=2000.
-ptmax2=2000.
-##output=full_analysis_moriond16v1_sync_v4_790_Inf
-##cut='(mass > 790.)'
-mkdir $output
+cuts=(0 1 2 3)
 
 
 
 ## merge MC
-./auto_plotter.py --selection cic,cicGenIso,cicNonGenIso \
-    --process GGJets\*EB\* \
-    --process GJets\*EB\* \
-    --file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4/output.root --move cicGenIso:cic,cicNonGenIso:cic \
-	--prescale GGJets\*EB\*:10 \
-	--cut "$cut" \
-    --histograms "mht30Mass>>mht30Mass(300,0.,3000.)" \
-    --histograms "mht60Mass>>mht60Mass(300,0.,3000.)" \
-    --histograms "nJets30>>nJets30(6,-0.5,5.5)" \
-    --histograms "nJets60>>nJets60(6,-0.5,5.5)" \
-    --histograms "leadPt>>leadPt(100,75.,$ptmax1)" \
-    --histograms "leadEta>>leadEta(50,-2.5,2.5)" \
-    --histograms "leadPhi>>leadPhi(64,-3.2,3.2)" \
-    --histograms "subleadPt>>subleadPt(100,0.,$ptmax2)" \
-    --histograms "subleadEta>>subleadEta(50,-2.5,2.5)" \
-    --histograms "subleadPhi>>subleadPhi(64,-3.2,3.2)" \
-    --output $output/output_mc_EBEB.root 
+for cut in "${cuts[@]}" ;do
+	echo $cut
+	if [[ $cut -eq 0 ]]; then
+		output=full_analysis_moriond16v1_sync_v4_0_730
+		cut='(mass < 730.)'
+	elif [[ $cut -eq 1 ]]; then
+		output=full_analysis_moriond16v1_sync_v4_500_730
+		cut='(mass > 500 && mass < 730.)'
+	elif [[ $cut -eq 2 ]]; then
+		output=full_analysis_moriond16v1_sync_v4_730_790
+		cut='(mass > 730 && mass < 790)'
+	elif [[ $cut -eq 3 ]]; then
+		output=full_analysis_moriond16v1_sync_v4_790_Inf
+		cut='(mass > 790.)'
+	fi
+	mkdir $output
+	./auto_plotter.py --selection cic,cicGenIso,cicNonGenIso \
+    	--load auto_plotter_histos_jet.json \
+		--process GGJets\*\* \
+    	--process GJets\*\* \
+    	--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4/output.root \
+		--move cicGenIso:cic,cicNonGenIso:cic \
+		--prescale GGJets\*\*:10 \
+		--cut "$cut" \
+    	--output $output/output_mc.root 
 
 
 
   ##data merge in Data
-./auto_plotter.py --selection cic \
-   --file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4_data/output.root \
-   --process Data\*EB\* \
-	--cut "$cut"\
-   --histograms "mht30Mass>>mht30Mass(300,0.,3000.)" \
-   --histograms "mht60Mass>>mht60Mass(300,0.,3000.)" \
-   --histograms "nJets30>>nJets30(6,-0.5,5.5)" \
-   --histograms "nJets60>>nJets60(6,-0.5,5.5)" \
-   --histograms "leadPt>>leadPt(100,75.,$ptmax1)" \
-   --histograms "leadEta>>leadEta(50,-2.5,2.5)" \
-   --histograms "leadPhi>>leadPhi(64,-3.2,3.2)" \
-   --histograms "subleadPt>>subleadPt(100,0.,$ptmax2)" \
-   --histograms "subleadEta>>subleadEta(50,-2.5,2.5)" \
-   --histograms "subleadPhi>>subleadPhi(64,-3.2,3.2)" \
-   --output $output/output_data_EBEB.root 
+	./auto_plotter.py --selection cic \
+    	--load auto_plotter_histos_jet.json \
+   		--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4_data/output.root \
+   		--process Data\*\* \
+		--cut "$cut"\
+		--output $output/output_data.root 
 
-hadd -f $output/output.root $output/output_data_EBEB.root $output/output_mc_EBEB.root  
-./basic_plots.py --load basic_plots.json -O /afs/cern.ch/user/m/mquittna/www/diphoton/$output/ --input-dir $output  --fudge $fudge --lumi 2.69 
-	canvEBEE->SaveAs(Form("/afs/cern.ch/user/m/mquittna/www/diphoton/bkg_decomposition_moriond16v1_sync_v4_data/%s.png", canvEBEE->GetName()));
+	hadd -f $output/output.root $output/output_data.root $output/output_mc.root  
+	./basic_plots.py --load basic_plots.json -O /afs/cern.ch/user/m/mquittna/www/diphoton/moriond16/$output/ --input-dir $output  --fudge $fudge --lumi 2.69
+done	
