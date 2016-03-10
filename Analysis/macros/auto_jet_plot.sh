ed81eefc68c058110ef203d9e2e8cd1b02552c5a
Milena Quittnat <milena.quittnat@cern.ch>
2016-03-10 23:34:10 +0100

    auto plotter

--- Analysis/macros/auto_jet_plot.sh
+++ Analysis/macros/auto_jet_plot.sh
@@ -10,39 +10,40 @@ cuts=(0 1 2 3)
 for cut in "${cuts[@]}" ;do
 	echo $cut
 	if [[ $cut -eq 0 ]]; then
-		output=full_analysis_moriond16v1_sync_v4_0_730
+		output=full_analysis_moriond16v1_sync_v5_extra_vars_0_730
 		cut='(mass < 730.)'
 	elif [[ $cut -eq 1 ]]; then
-		output=full_analysis_moriond16v1_sync_v4_500_730
+		output=full_analysis_moriond16v1_sync_v5_extra_vars_500_730
 		cut='(mass > 500 && mass < 730.)'
 	elif [[ $cut -eq 2 ]]; then
-		output=full_analysis_moriond16v1_sync_v4_730_790
+		output=full_analysis_moriond16v1_sync_v5_extra_vars_730_790
 		cut='(mass > 730 && mass < 790)'
 	elif [[ $cut -eq 3 ]]; then
-		output=full_analysis_moriond16v1_sync_v4_790_Inf
+		output=full_analysis_moriond16v1_sync_v5_extra_vars_790_Inf
 		cut='(mass > 790.)'
 	fi
-	mkdir $output
-	./auto_plotter.py --selection cic,cicGenIso,cicNonGenIso \
-    	--load auto_plotter_histos_jet.json \
-		--process GGJets\*\* \
-    	--process GJets\*\* \
-    	--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4/output.root \
-		--move cicGenIso:cic,cicNonGenIso:cic \
-		--prescale GGJets\*\*:10 \
-		--cut "$cut" \
-    	--output $output/output_mc.root 
+#	mkdir $output
+#	./auto_plotter.py --selection cic,cicGenIso,cicNonGenIso \
+#    	--load auto_plotter_histos_jet.json \
+#		--process GGJets\*\* \
+#    	--process GJets\*\* \
+#    	--file /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v5_extra_vars_data/output.root \
+#
+#		--move cicGenIso:cic,cicNonGenIso:cic \
+#		--prescale GGJets\*\*:10 \
+#		--cut "$cut" \
+#    	--output $output/output_mc.root 
 
 
 
   ##data merge in Data
-	./auto_plotter.py --selection cic \
-    	--load auto_plotter_histos_jet.json \
-   		--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4_data/output.root \
-   		--process Data\*\* \
-		--cut "$cut"\
-		--output $output/output_data.root 
+#	./auto_plotter.py --selection cic \
+#    	--load auto_plotter_histos_jet.json \
+#   		--file /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v5_extra_vars_data/output.root \
+#   		--process Data\*\* \
+#		--cut "$cut"\
+#		--output $output/output_data.root 
 
-	hadd -f $output/output.root $output/output_data.root $output/output_mc.root  
+#	hadd -f $output/output.root $output/output_data.root $output/output_mc.root
 	./basic_plots.py --load basic_plots.json -O /afs/cern.ch/user/m/mquittna/www/diphoton/moriond16/$output/ --input-dir $output  --fudge $fudge --lumi 2.69
 done	
--- Analysis/macros/auto_jeteta_plot.sh
+++ Analysis/macros/auto_jeteta_plot.sh
@@ -17,22 +17,22 @@ cuts=(0 1 2 3)
 for cut in "${cuts[@]}" ;do
 	echo $cut
 	if [[ $cut -eq 0 ]]; then
-		output=full_analysis_moriond16v1_sync_v4_0_730_min2Jets
+		output=full_analysis_moriond16v1_sync_v5_extra_vars_0_730_min2Jets
 		cut30='(nJets30 >1 && mass < 730.)'
 		cut60='(nJets60 >1 && mass < 730.)'
 		cut301='(nJets30 >0 && mass < 730.)'
 	elif [[ $cut -eq 1 ]]; then
-		output=full_analysis_moriond16v1_sync_v4_500_790_min2Jets
+		output=full_analysis_moriond16v1_sync_v5_extra_vars_500_790_min2Jets
 		cut30='(nJets30 >1 && mass > 500. && mass < 730.)'
 		cut60='(nJets60 >1 && mass > 500. && mass < 730.)'
 		cut301='(nJets30 >0  && mass > 500. && mass < 730.)'
 	elif [[ $cut -eq 2 ]]; then
-		output=full_analysis_moriond16v1_sync_v4_730_790_min2Jets
+		output=full_analysis_moriond16v1_sync_v5_extra_vars_730_790_min2Jets
 		cut30='(nJets30 >1 && mass > 730 && mass < 790)'
 		cut60='(nJets60 >1 && mass > 730. && mass < 790.)'
 		cut301='(nJets30 >0 && mass > 730. && mass < 790.)'
 	elif [[ $cut -eq 3 ]]; then
-		output=full_analysis_moriond16v1_sync_v4_790_Inf_min2Jets
+		output=full_analysis_moriond16v1_sync_v5_extra_vars_790_Inf_min2Jets
 		cut30='(nJets30 >1 && mass > 790.)'
 		cut60='(nJets60 >1 && mass > 790.)'
 		cut301='(nJets30 >0 && mass > 790.)'
@@ -40,82 +40,81 @@ for cut in "${cuts[@]}" ;do
 	mkdir $output
 
 
-	./auto_plotter.py --selection cic,cicGenIso,cicNonGenIso \
-		--process GGJets\*\* \
-		--process GJets\*\* \
-		--process GJets\*\* \
-		--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4/output.root --move cicGenIso:cic,cicNonGenIso:cic \
-		--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4/output.root --move cicGenIso:cic,cicNonGenIso:cic \
-		--prescale GGJets\*\*:10 \
-		--cut "$cut30" \
-		--histograms "abs(jet2Eta-jet1Eta)>>deltaEta(10,0.0,5.0)" \
-		--histograms "mht30CleanMass>>mht30CleanMass(300,$minmass,$maxmass)" \
-		--histograms "dijet30CleanMass>>dijet30CleanMass(300,$minmass,$maxmass)" \
-		--histograms "mht30Mass>>mht30Mass(300,0.,$maxmass2)" \
-		--histograms "(rapidity-(jet2Eta+jet1Eta)/2.)>>zeppenfeld(10,-5.,5.)" \
-		--histograms "jet2Pt>>jet2Pt(200,0.,$ptmax2)" \
-		--histograms "jet2Eta>>jet2Eta(50,-2.5,2.5)" \
-		--histograms "jet2Phi>>jet2Phi(64,-3.2,3.2)" \
-		--output $output/output_mc30.root 
+#	./auto_plotter.py --selection cic,cicGenIso,cicNonGenIso \
+#		--process GGJets\*\* \
+#		--process GJets\*\* \
+#		--process GJets\*\* \
+#		--file /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v5_extra_vars_data/output.root --move cicGenIso:cic,cicNonGenIso:cic \
+#		--prescale GGJets\*\*:10 \
+#		--cut "$cut30" \
+#		--histograms "abs(jet2Eta-jet1Eta)>>deltaEta(10,0.0,5.0)" \
+#		--histograms "mht30CleanMass>>mht30CleanMass(300,$minmass,$maxmass)" \
+#		--histograms "dijet30CleanMass>>dijet30CleanMass(300,$minmass,$maxmass)" \
+#		--histograms "mht30Mass>>mht30Mass(300,0.,$maxmass2)" \
+#		--histograms "(rapidity-(jet2Eta+jet1Eta)/2.)>>zeppenfeld(10,-5.,5.)" \
+#		--histograms "jet2Pt>>jet2Pt(200,0.,$ptmax2)" \
+#		--histograms "jet2Eta>>jet2Eta(50,-2.5,2.5)" \
+#		--histograms "jet2Phi>>jet2Phi(64,-3.2,3.2)" \
+#		--output $output/output_mc30.root 
 
-	./auto_plotter.py --selection cic \
-		--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4_data/output.root \
-		--process Data\*\* \
-		--cut "$cut30"\
-		--histograms "abs(jet2Eta-jet1Eta)>>deltaEta(10,0.0,5.0)" \
-		--histograms "mht30CleanMass>>mht30CleanMass(300,$minmass,$maxmass)" \
-		--histograms "dijet30CleanMass>>dijet30CleanMass(300,$minmass,$maxmass)" \
-		--histograms "mht30Mass>>mht30Mass(300,0.,$maxmass2)" \
-		--histograms "(rapidity-(jet2Eta+jet1Eta)/2.)>>zeppenfeld(10,-5.,5.)" \
-		--histograms "jet2Pt>>jet2Pt(200,0.,$ptmax2)" \
-		--histograms "jet2Eta>>jet2Eta(50,-2.5,2.5)" \
-		--histograms "jet2Phi>>jet2Phi(64,-3.2,3.2)" \
-		--output $output/output_data30.root 
+#	./auto_plotter.py --selection cic \
+#		--file /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v5_extra_vars_data/output.root \
+#		--process Data\*\* \
+#		--cut "$cut30"\
+#		--histograms "abs(jet2Eta-jet1Eta)>>deltaEta(10,0.0,5.0)" \
+#		--histograms "mht30CleanMass>>mht30CleanMass(300,$minmass,$maxmass)" \
+#		--histograms "dijet30CleanMass>>dijet30CleanMass(300,$minmass,$maxmass)" \
+#		--histograms "mht30Mass>>mht30Mass(300,0.,$maxmass2)" \
+#		--histograms "(rapidity-(jet2Eta+jet1Eta)/2.)>>zeppenfeld(10,-5.,5.)" \
+#		--histograms "jet2Pt>>jet2Pt(200,0.,$ptmax2)" \
+#		--histograms "jet2Eta>>jet2Eta(50,-2.5,2.5)" \
+#		--histograms "jet2Phi>>jet2Phi(64,-3.2,3.2)" \
+#		--output $output/output_data30.root 
 
-	./auto_plotter.py --selection cic,cicGenIso,cicNonGenIso \
-		--process GGJets\*\* \
-		--process GJets\*\* \
-		--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4/output.root --move cicGenIso:cic,cicNonGenIso:cic \
-		--prescale GGJets\*\*:10 \
-		--cut "$cut301" \
-		--histograms "jet1Pt>>jet1Pt(200,0.,$ptmax1)" \
-		--histograms "jet1Eta>>jet1Eta(50,-2.5,2.5)" \
-		--histograms "jet1Phi>>jet1Phi(64,-3.2,3.2)" \
-		--output $output/output_mc30_1.root 
+#	./auto_plotter.py --selection cic,cicGenIso,cicNonGenIso \
+#		--process GGJets\*\* \
+#		--process GJets\*\* \
+#		--file /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v5_extra_vars_data/output.root --move cicGenIso:cic,cicNonGenIso:cic \
+#		--prescale GGJets\*\*:10 \
+#		--cut "$cut301" \
+#		--histograms "jet1Pt>>jet1Pt(200,0.,$ptmax1)" \
+#		--histograms "jet1Eta>>jet1Eta(50,-2.5,2.5)" \
+#		--histograms "jet1Phi>>jet1Phi(64,-3.2,3.2)" \
+#		--output $output/output_mc30_1.root 
 		
-	./auto_plotter.py --selection cic \
-	  	--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4_data/output.root \
-	  	--process Data\*\* \
-		--cut "$cut301"\
-		--histograms "jet1Pt>>jet1Pt(200,0.,$ptmax1)" \
-		--histograms "jet2Eta>>jet1Eta(50,-2.5,2.5)" \
-		--histograms "jet1Phi>>jet1Phi(64,-3.2,3.2)" \
-		--output $output/output_data31_1.root 
+#	./auto_plotter.py --selection cic \
+#	  	--file /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v5_extra_vars_data/output.root \
+#	  	--process Data\*\* \
+#		--cut "$cut301"\
+#		--histograms "jet1Pt>>jet1Pt(200,0.,$ptmax1)" \
+#		--histograms "jet2Eta>>jet1Eta(50,-2.5,2.5)" \
+#		--histograms "jet1Phi>>jet1Phi(64,-3.2,3.2)" \
+#		--output $output/output_data30_1.root 
+#
 
-
-	./auto_plotter.py --selection cic,cicGenIso,cicNonGenIso \
-	   	--process GGJets\*\* \
-	   	--process GJets\*\* \
-	   	--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4/output.root --move cicGenIso:cic,cicNonGenIso:cic \
-	   	--prescale GGJets\*\*:10 \
-	   	--cut "$cut60" \
-		--histograms "mht60CleanMass>>mht60CleanMass(300,$minmass,$maxmass)" \
-		--histograms "dijet60CleanMass>>dijet60CleanMass(300,$minmass,$maxmass)" \
-		--histograms "mht60Mass>>mht60Mass(300,0.,$maxmass)" \
-		--output $output/output_mc60.root 
+#	./auto_plotter.py --selection cic,cicGenIso,cicNonGenIso \
+#	   	--process GGJets\*\* \
+#	   	--process GJets\*\* \
+#	   	--file /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v5_extra_vars_data/output.root --move cicGenIso:cic,cicNonGenIso:cic \
+#	   	--prescale GGJets\*\*:10 \
+#	   	--cut "$cut60" \
+#		--histograms "mht60CleanMass>>mht60CleanMass(300,$minmass,$maxmass)" \
+#		--histograms "dijet60CleanMass>>dijet60CleanMass(300,$minmass,$maxmass)" \
+#		--histograms "mht60Mass>>mht60Mass(300,0.,$maxmass)" \
+#		--output $output/output_mc60.root 
 
  
-	./auto_plotter.py --selection cic \
-		--file  /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v4_data/output.root \
-		--process Data\*\* \
-		--cut "$cut60"\
-		--histograms "mht60CleanMass>>mht60CleanMass(300,$minmass,$maxmass)" \
-		--histograms "dijet60CleanMass>>dijet60CleanMass(300,$minmass,$maxmass)" \
-		--histograms "mht60Mass>>mht60Mass(300,0.,$maxmass)" \
-		--output $output/output_data60.root 
+#	./auto_plotter.py --selection cic \
+#		--file /afs/cern.ch/user/m/musella/public/workspace/exo/full_analysis_moriond16v1_sync_v5_extra_vars_data/output.root \
+#		--process Data\*\* \
+#		--cut "$cut60"\
+#		--histograms "mht60CleanMass>>mht60CleanMass(300,$minmass,$maxmass)" \
+#		--histograms "dijet60CleanMass>>dijet60CleanMass(300,$minmass,$maxmass)" \
+#		--histograms "mht60Mass>>mht60Mass(300,0.,$maxmass)" \
+#		--output $output/output_data60.root 
 
 
 
-	hadd -f $output/output.root $output/output_data30_1.root $output/output_mc30_1.root  $output/output_data30.root $output/output_mc30.root  $output/output_data60.root $output/output_mc60.root 
+#	hadd -f $output/output.root $output/output_data30_1.root $output/output_mc30_1.root  $output/output_data30.root $output/output_mc30.root  $output/output_data60.root $output/output_mc60.root 
 	./basic_plots.py --load basic_plots.json -O /afs/cern.ch/user/m/mquittna/www/diphoton/$output/ --input-dir $output --fudge $fudge --lumi 2.69
 done
