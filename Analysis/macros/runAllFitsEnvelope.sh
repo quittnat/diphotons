36296047b267e1f1cdb1419e11cd384913cb2ed7
Milena Quittnat <milena.quittnat@cern.ch>
2016-05-19 18:22:13 +0200

    before change to run over different masses

--- Analysis/macros/runAllFitsEnvelope.sh
+++ Analysis/macros/runAllFitsEnvelope.sh
@@ -15,10 +15,10 @@ bkgfile="/afs/cern.ch/user/m/mquittna/public/envelope_EXO/output_flashggFinalFit
 ###mkdir $folder
 
 cp $bkgfile $folder/.
-./combine_maker.sh $file2 --luminosity 2.56 --lumi 2.56 --generate-signal --fit-name cic2 --generate-datacard --rescale-signal-to 1e-3 --parametric-signal nominalWSwithSmear_k001_m500to998.root --parametric-signal-acceptance acceptance_pu.json --background-root-file $bkgfile --use-envelope
+./combine_maker.sh $file2 --luminosity 2.56 --lumi 2.56 --generate-signal --fit-name cic2 --generate-datacard --rescale-signal-to 1e-3 --parametric-signal srm://t3se01.psi.ch/pnfs/psi.ch/cms/trivcat/store/user/mquittna/nominalWSwithSmear_k001_m500to998.root --parametric-signal-acceptance acceptance_pu.json --background-root-file $bkgfile --use-envelope
 ##./combine_maker.sh $file2 --luminosity 2.56 --lumi 2.56 --generate-signal --fit-name cic2 --generate-datacard --rescale-signal-to 1e-3 --parametric-signal nominalWSwithSmear_k001_m500to998.root --parametric-signal-acceptance acceptance_pu.json --background-root-file $bkgfile 
 wait
-cd $folder
+#cd $folder
 ###combine -L libdiphotonsUtils -t -1 -M MultiDimFit --expectSignal 7 -n _k001fit -m 750 -v 3 --saveWorkspace --saveSpecifiedIndex pdfindex_EBEB --saveToys --freezeNuisances pdfindex_EBEB --setPhysicsModelParameters pdfindex_EBEB=0,r=0 datacard_full_analysis_spring15_7415v2_sync_v5_cic2_default_shapes_grav_001_750.txt |tee output_MultiDimFit.txt
 
 ###combine -L libdiphotonsUtils -t -1 -M MultiDimFit --expectSignal 7 -n _k01fittoysFreq -m 750 -v 3 --saveWorkspace --saveSpecifiedIndex pdfindex_EBEB,pdfindex_EBEE --toysFrequentist --saveToys --setPhysicsModelParameters r=0 datacard_full_analysis_spring15_7415v2_sync_v5_cic2_default_shapes_grav_01_750.txt |tee output_MultiDimFit_toysFrequentist.txt
